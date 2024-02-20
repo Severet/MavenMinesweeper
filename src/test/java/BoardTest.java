ac6745e38308bcdab78d87277fd75b6c6efef10f
Severet <severet.reading@gmail.com>
2024-02-20 11:48:35 +0000

    further board tests

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,3 +1,4 @@
+import com.sun.source.tree.AssertTree;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,9 +9,24 @@ class BoardTest {
         Assertions.assertEquals(5, game.getSize(),"Board size is not 5");
     }
     @Test
-    void mineDistributionTest() {
+    void mineTests() {
         Board game = new Board(20);
         game.distributeMines(75);
         Assertions.assertEquals(75, game.getMinecount(), "Mines are not 10");
+
+        Tile[][] grid = game.getTiles();
+        grid[2][2].setMine(true);
+        grid[3][3].setMine(false);
+
+        game.revealTile(grid[3][3]);
+
+        game.flagTile(grid[2][2]);
+
+        game.revealTile(grid[2][2]);
+
+        game.flagTile(grid[2][2]);
+
+        game.revealTile(grid[2][2]);
+
     }
 }

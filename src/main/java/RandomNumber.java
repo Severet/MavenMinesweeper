542ae97394d00955f8f11a3d87c298275268c251
Severet <severet.reading@gmail.com>
2024-02-20 13:48:42 +0000

    Added private constructor for RandomNumber class

--- src/main/java/RandomNumber.java
+++ src/main/java/RandomNumber.java
@@ -2,6 +2,9 @@ import java.util.Random;
 
 public class RandomNumber {
     private static final Random r = new Random();
+    private RandomNumber() {
+        throw new IllegalStateException("Utility class");
+    }
     public static int generateInt(int limit) {
         return r.nextInt(limit);
     }
